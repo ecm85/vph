e5a90e793f7f6ef1a49864fd92942782afbffdeb
Ezra <ezramcnichols@gmail.com>
2020-10-23 15:57:56 -0500

    Redirecting to new site.

--- Vph.Pl/Controllers/HomeController.cs
+++ Vph.Pl/Controllers/HomeController.cs
@@ -29,7 +29,7 @@ namespace Vph.Pl.Controllers
 
         Authenticator CreateAuthenticator()
         {
-            var redirectUrl = "http://www.stormtide.net/vph/Home/Callback";
+            var redirectUrl = "https://vph.stormtide.net/Home/Callback";
             var config = new RestSharp.Portable.OAuth2.Configuration.RuntimeClientConfiguration
             {
                 IsEnabled = false,
